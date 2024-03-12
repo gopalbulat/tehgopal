8669c3f782fe0d3326667c421b5eb8ef00c2bd79
gopalbulat <163063375+gopalbulat@users.noreply.github.com>
2024-03-12 17:52:18 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const count = require('text-count');
+
+function gopalbulat() {
+  const text = "G O P A L B U L A T"
+  return {
+    text,
+    length: count.charCount(text)
+  }
+}
+
+
+module.exports = Gopalbulat
